31ccdc55654e8064d01ab31e79d3f0032734bcf4
Joseph Adams <josephdadams@gmail.com>
2025-04-23 11:41:36 -0400

    cleanup, change default window size

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,5 @@
 import { BrowserWindow } from 'electron'
 import * as path from 'path'
-
 import { fileURLToPath } from 'url'
 
 let settingsWindow: BrowserWindow | null = null
@@ -11,23 +10,19 @@ export default function createSettingsWindow() {
         return
     }
 
-    // Define __dirname in ESM
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = path.dirname(__filename)
 
     settingsWindow = new BrowserWindow({
-        width: 400,
-        height: 600,
+        width: 300,
+        height: 300,
         resizable: false,
         webPreferences: {
-            preload: path.join(__dirname, '../dist/preload.js'),
+            preload: path.join(__dirname, 'preload.js'),
         },
     })
 
     settingsWindow.loadFile(path.join(__dirname, '../public/settings.html'))
-
-    // Open the DevTools for debugging
-    //settingsWindow.webContents.openDevTools()
 
     settingsWindow.on('closed', () => {
         settingsWindow = null
